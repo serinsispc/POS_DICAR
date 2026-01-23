4d51e1c50518e59661f01d95686293d4fa1f09f7
Emiliano Polania <81882991+serinsispc@users.noreply.github.com>
2026-01-23 15:41:46 -0500

    23-01-2026  3.41pm

--- POS SERINSIS PC 2022/Clases/VariablesPublicas.cs
+++ POS SERINSIS PC 2022/Clases/VariablesPublicas.cs
@@ -9,7 +9,7 @@ namespace Invenpol_Parqueadero_Motos.Clases
 {
     public class VariablesPublicas
     {
-        public static string version = "1.1";
+        public static string version = "1.2";
 
         public static string nombreVendedor = "";
         public static bool Gramera = false;
